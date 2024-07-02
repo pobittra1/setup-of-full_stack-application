802048423bd3e11083fa53ef4864914320807f55
pobi <pobittrachandraroy2001@gmail.com>
2024-07-02 13:05:27 +0600

    student enrolled validation check in service file

--- src/app/modules/enrolledCourse/enrolledCourse.controller.ts
+++ src/app/modules/enrolledCourse/enrolledCourse.controller.ts
@@ -1,13 +1,22 @@
 import httpStatus from "http-status";
 import catchAsync from "../../utils/catchAsync";
 import sendResponse from "../../utils/sendResponse";
+import { JwtPayload } from "jsonwebtoken";
 import { enrolledCourseService } from "./enrolledCourse.service";
-
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      user: JwtPayload;
+    }
+  }
+}
 //destructure services
 const { createEnrolledCourseIntoDB } = enrolledCourseService;
 
 const createEnrolledCourse = catchAsync(async (req, res) => {
-  const result = await createEnrolledCourseIntoDB();
+  const userId = req.user.userId;
+  const result = await createEnrolledCourseIntoDB(userId, req.body);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
--- src/app/modules/enrolledCourse/enrolledCourse.service.ts
+++ src/app/modules/enrolledCourse/enrolledCourse.service.ts
@@ -1,4 +1,48 @@
-const createEnrolledCourseIntoDB = async () => {};
+import httpStatus from "http-status";
+import { OfferedCourse } from "../offeredCourse/offeredCourse.model";
+import { TEnrolledCourse } from "./enrolledCourse.interface";
+import AppError from "../../config/error/AppError";
+import EnrolledCourse from "./enrolledCourse.model";
+import Student from "../student/student.model";
+
+const createEnrolledCourseIntoDB = async (
+  userId: string,
+  payload: TEnrolledCourse
+) => {
+  /**
+   * Step1: Check if the offered cousres is exists
+   * Step2: Check if the student is already enrolled
+   * Step3: Check if the max credits exceed
+   * Step4: Create an enrolled course
+   */
+
+  const { offeredCourse } = payload;
+
+  const isOfferedCourseExists = await OfferedCourse.findById(offeredCourse);
+
+  if (!isOfferedCourseExists) {
+    throw new AppError(httpStatus.NOT_FOUND, "Offered course not found !");
+  }
+
+  if (isOfferedCourseExists.maxCapacity <= 0) {
+    throw new AppError(httpStatus.BAD_GATEWAY, "Room is full !");
+  }
+  const student = await Student.findOne({ id: userId }, { _id: 1 });
+
+  if (!student) {
+    throw new AppError(httpStatus.NOT_FOUND, "Student not found !");
+  }
+
+  const isStudentAlreadyEnrolled = await EnrolledCourse.findOne({
+    semesterRegistration: isOfferedCourseExists?.semesterRegistration,
+    offeredCourse,
+    student: student._id,
+  });
+
+  if (isStudentAlreadyEnrolled) {
+    throw new AppError(httpStatus.CONFLICT, "Student is already enrolled !");
+  }
+};
 
 export const enrolledCourseService = {
   createEnrolledCourseIntoDB,
